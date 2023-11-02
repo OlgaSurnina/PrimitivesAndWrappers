f6b4536c0415b4bb013c5970e766913f982f4610
Olga Surnina <veehja88@gmail.com>
2023-11-02 21:36:29 +0300

    Initial commit

---
+++ src/main/java/Primitives.java
@@ -0,0 +1,38 @@
+public class Primitives {
+
+
+    public static void main(String[] args) {
+
+        byte myByte;
+        myByte = 121;
+        System.out.println(myByte);
+
+        short myShort;
+        myShort = 31767;
+        System.out.println(myShort);
+
+        int myInt;
+        myInt = 2147443647;
+        System.out.println(myInt);
+
+        long myLong;
+        myLong = 922337234554775807L;
+        System.out.println(myLong);
+
+        float myFloat;
+        myFloat = 3.14f;
+        System.out.println(myFloat);
+
+        double myDouble;
+        myDouble = 3.14159;
+        System.out.println(myDouble);
+
+        char myChar;
+        myChar = 'X';
+        System.out.println(myChar);
+
+        boolean myBoolean;
+        myBoolean = false;
+        System.out.println(myBoolean);
+    }
+}
---
+++ src/main/java/Wrappers.java
@@ -0,0 +1,11 @@
+public class Wrappers {
+
+
+    public static void main(String[] args) {
+
+        String myString = "666";
+        Integer i = Integer.parseInt(myString);
+
+        System.out.println(i);
+    }
+}
